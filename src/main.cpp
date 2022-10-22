a47f9ea8a99398f2daf4238889b5eb2dc3e7d9c3
unsigned-long <3079625093@qq.com>
2022-10-22 15:24:19 +0800

    add readme.md

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
         using namespace ns_viewer;
         SceneViewer viewer("../scene-shot");
         {
-            CubePlane plane(0.0f, 0.0f, -30.0f, 1.0f, 2.0f, 3.0f);
+            CubePlane plane(0.0f, 0.0f, -30.0f, 1.0f, 2.0f, 1.0f);
             viewer.AddCubePlane(plane, true, 0.5);
             viewer.AddFeatures(plane.GenerateFeatures(10, CubePlane::ALL), 6.0, 0.6);
             viewer.AddPose(plane.LtoW);
@@ -16,7 +16,8 @@ int main(int argc, char **argv) {
             Eigen::AngleAxisf r1(-M_PI_4, Eigen::Vector3f(0, 0, 1));
             Eigen::AngleAxisf r2(+M_PI_2, Eigen::Vector3f(1, 0, 0));
             auto rot = (r2 * r1).toRotationMatrix();
-            viewer.AddCamera(Posef(rot.inverse(), Eigen::Vector3f(2, 1, 3)));
+            auto CtoW = Posef(rot.inverse(), Eigen::Vector3f(2, 1, 1));
+            viewer.AddCamera(CtoW, Colour::Black());
         }
 
         // viewer.RunSingleThread();
